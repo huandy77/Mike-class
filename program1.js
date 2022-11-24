3f27ff9e951b56e504461dc942e8b335206d7bb6
huandy77 <118422736+huandy77@users.noreply.github.com>
2022-11-24 14:06:00 +0900

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,12 @@
+
+let Name = "An"
+let Classes = ["CF", "ST", "ACC", "GVT", "QBA"]
+let String = "hello, my name is " + Name + " and I am taking " 
+for(let Index in Classes){
+if (Index < Classes.length - 1){
+    String += Classes[Index] + ", ";
+    } else {
+    String += "and " + Classes[Index] + ".";
+    }
+}
+console.log(String)
